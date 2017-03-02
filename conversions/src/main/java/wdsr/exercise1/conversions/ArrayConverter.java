7010513ed53cc6af56b1af36140bfa853dd203ef
radekkania <radekkania7@wp.pl>
2017-03-02 12:50:15 +0100

    log added in ArrayConverter\ convertToInts, modification of log4j configuration file

--- conversions/src/main/java/wdsr/exercise1/conversions/ArrayConverter.java
+++ conversions/src/main/java/wdsr/exercise1/conversions/ArrayConverter.java
@@ -2,11 +2,19 @@ package wdsr.exercise1.conversions;
 
 import java.util.Arrays;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 /**
  * Created by Marek on 14.02.2016.
  */
 public class ArrayConverter {
+	private static final Logger log = LogManager.getLogger();
+	
     public int[] convertToInts(String[] strings) {
-        return Arrays.stream(strings).mapToInt(Integer::valueOf).toArray();
+    	log.debug("convertToInts entered [strings={}]", Arrays.toString(strings));
+    	int[] integers = Arrays.stream(strings).mapToInt(Integer::valueOf).toArray();
+    	log.debug("convertToInts derived [integers={}]", Arrays.toString(integers));
+        return integers;
     }
 }
